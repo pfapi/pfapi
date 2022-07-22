eb8df62953f1e8efce421d3e5204d14e7b9df503
Sam Wen <samw@ritani.com>
2022-07-22 19:06:18 -0400

    update

--- server/lib/index.js
+++ server/lib/index.js
@@ -4,8 +4,8 @@ const fs = require('fs')
 
 const { pfapi_path, helpers_path}  = require('./local-paths')
 
-const pfapi = pfapi_path && fs.existsSync(pfapi_path) ? require(pfapi_path) : require('strapi-pfapi');
-const helpers = helpers_path && fs.existsSync(helpers_path) ? require(helpers_path) : require('@iamsamwen/strapi-query-helpers');
+const pfapi = pfapi_path && fs.existsSync(pfapi_path) ? require(pfapi_path) : require('@pfapi/core');
+const helpers = helpers_path && fs.existsSync(helpers_path) ? require(helpers_path) : require('@pfapi/query-helpers');
 
 module.exports = {
     ...pfapi,
--- server/lib/local-paths.js
+++ server/lib/local-paths.js
@@ -4,6 +4,6 @@
  * setup for the convenience of development
  */
 module.exports = {
-    pfapi_path: '/Users/sam/iamsamwen/strapi-pfapi/src',
-    helpers_path: '/Users/sam/iamsamwen/strapi-query-helpers/src',
+    pfapi_path: '/opt/pfapi/pfapi-core/src',
+    helpers_path: '/opt/pfapi/query-helpers/src',
 };
