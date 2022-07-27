11e49553ba2858107fc3678fa3b470bae5d5979c
Sam Wen <samw@ritani.com>
2022-07-26 20:34:11 -0400

    update

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// const fs = require('fs')
-
-// const { pfapi_path, helpers_path}  = require('./local-paths')
-
-// const pfapi = pfapi_path && fs.existsSync(pfapi_path) ? require(pfapi_path) : require('@pfapi/core');
-// const helpers = helpers_path && fs.existsSync(helpers_path) ? require(helpers_path) : require('@pfapi/query-helpers');
-
-// module.exports = {
-//     ...pfapi,
-//     ...helpers
-// }
-
 module.exports = {
     ...require('@pfapi/core'),
     ...require('@pfapi/query-helpers')
--- server/lib/local-paths.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * setup for the convenience of development
- */
-module.exports = {
-    pfapi_path: '/opt/pfapi/pfapi-core/src',
-    helpers_path: '/opt/pfapi/query-helpers/src',
-};
