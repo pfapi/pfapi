654387821b9278691a14df391f0005a278cbe950
Sam Wen <samw@ritani.com>
2022-08-01 16:43:23 -0400

    update

--- packages/strapi-app/config/database.js
+++ packages/strapi-app/config/database.js
@@ -1,32 +1,11 @@
 const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DB', 'sqlite')
-  if (client === 'mysql') {
-    return {
-      connection: {
-        client,
-        connection: {
-          host: 'localhost',
-          port: 3306,
-          database: 'pfapi_test',
-          user: 'root',
-          password: 'root',
-          ssl: false,
-        },
-      },
-    };
-  } else if (client === 'sqlite') {
-    return {
-      connection: {
-        client,
-        connection: {
-          filename: path.join(__dirname, '..', '.tmp/data.db'),
-        },
-        useNullAsDefault: true,
-      },
-    };
-  } else {
-    throw new Error(`database ${client} not supported`)
-  }
-}
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: path.join(__dirname, '..', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+  },
+});
